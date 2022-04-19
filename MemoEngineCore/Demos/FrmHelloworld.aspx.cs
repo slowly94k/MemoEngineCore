456ffa9bd27b6d542c372e00bd03d5968c016fb3
hwkim <slowly94k@naver.com>
2022-04-19 16:23:01 +0900

    FrmHelloWorld.aspx [실습] ASP.NET 웹 프로젝트 작성해보기

--- MemoEngineCore/Demos/FrmHelloworld.aspx.cs
+++ MemoEngineCore/Demos/FrmHelloworld.aspx.cs
@@ -13,5 +13,11 @@ namespace MemoEngineCore
         {
 
         }
+
+        protected void btnClick_Click(object sender, EventArgs e)
+        {
+            lbDisplay.Text = txtInput.Text;
+            txtInput.Text = String.Empty;
+        }
     }
 }
